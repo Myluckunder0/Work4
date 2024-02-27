a152fc5c7f5f4ba9a4c527a7691aafdf4df212af
Myluckunder0 <inthira_ta@hotmail.com>
2024-02-27 18:31:27 +0700

    Create Work4.py

---
+++ Work4.py
@@ -0,0 +1,11 @@
+print("รายการสินค้า")
+a=float(input("150: "))
+b=float(input("105: "))
+c=float(input("150: "))
+d=float(input("120: "))
+e=float(input("20: "))
+f=float(input("21.50: "))
+sall=a+b+c+d+e+f
+cash=int(input("1000: "))
+chan=sall
+print("เงินทอน:", chan)
